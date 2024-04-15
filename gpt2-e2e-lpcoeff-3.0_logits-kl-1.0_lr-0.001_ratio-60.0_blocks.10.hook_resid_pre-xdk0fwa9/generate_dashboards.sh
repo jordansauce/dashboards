90dd652afa660558e0d336a8d67f4a66d358880b
jordansauce <nadroj@live.com.au>
2024-04-15 08:56:18 +0000

    Started generating dashboards from the UMAP SAEs 2

---
+++ gpt2-e2e-lpcoeff-3.0_logits-kl-1.0_lr-0.001_ratio-60.0_blocks.10.hook_resid_pre-xdk0fwa9/generate_dashboards.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=12:00:00
+#SBATCH --partition=single
+#SBATCH --job-name=dashboards-gpt2-e2e-2
+#SBATCH --mem=40G
+
+# Recommended way if you want to enable gcc version 10 for the "sbatch" session n 
+source /opt/rh/devtoolset-10/enable
+conda init
+
+conda activate py312
+
+pwd
+
+python /data/jordan_tensor/sparsify/sparsify/scripts/generate_dashboards.py /data/jordan_tensor/sparsify/sparsify/scripts/dashboards.yaml samples_200000.pt
